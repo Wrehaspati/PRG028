7d5f2803dc6a3b44ad90363de64ce6a0df9dcf88
Rayy <96776929+goznt@users.noreply.github.com>
2023-06-18 13:03:10 +0800

    edit desain

--- elearning/resources/views/auth/forgot-password.blade.php
+++ elearning/resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,15 @@
 <x-guest-layout>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <div class="row d-flex justify-content-center align-items-center h-100"
+        style="width : 150px; margin-left : 8rem; margin-bottom : 0rem;">
+        <img src="https://ouch-cdn2.icons8.com/QoHUrcO9_hEI_12K14-Pgzn9WLXMCurJFFBriPEyv80/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNTgx/LzUwMmFiNDc0LTZh/MzAtNDAzZS05NDBi/LTZiMGRmMGRiMWJi/NS5wbmc.png"
+            alt="Sample image">
+    </div>
+    <br>
+    <h3>Forgot Password?</h3>
+
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -12,7 +23,8 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
